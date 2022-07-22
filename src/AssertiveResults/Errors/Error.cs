831f1aee703c77d7298a04f5fc98a0e6c763a8c0
Taufan Augusta <taufan.augusta@protonmail.com>
2022-07-22 19:26:21 +0700

    refactor: Error ctor

--- src/AssertiveResults/Errors/Error.cs
+++ src/AssertiveResults/Errors/Error.cs
@@ -3,5 +3,15 @@ namespace AssertiveResults.Errors
     public class Error
     {
         public string Message { get; internal set; }
+
+        public Error()
+        {
+            Message = "Unspecified error has occurred.";
+        }
+
+        public Error(string errorMessage)
+        {
+            Message = errorMessage;
+        }
     }
 }
