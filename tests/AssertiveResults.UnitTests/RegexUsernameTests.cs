d6d3415107ae372eed119e942c897ce00119a75d
Taufan Augusta <taufan.augusta@protonmail.com>
2022-07-26 03:52:32 +0700

    tests: username validation

---
+++ tests/AssertiveResults.UnitTests/RegexUsernameTests.cs
@@ -0,0 +1,50 @@
+using AssertiveResults;
+
+namespace AssertiveResults.UnitTests;
+
+public class RegexUsernameTests
+{
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData("..")]
+    [InlineData("._")]
+    [InlineData("__")]
+    [InlineData("xy")]
+    [InlineData(".user")]
+    [InlineData("_user")]
+    [InlineData("user.")]
+    [InlineData("user_")]
+    [InlineData("user..name")]
+    [InlineData("user._name")]
+    [InlineData("user_.name")]
+    [InlineData("user__name")]
+    [InlineData("WayTooLongPassword")]
+    public void InvalidUsernameTest(string username)
+    {
+        var result = Assertive.Result()
+            .Assert(x => x.Regex.Must(username).Validates.Username(3, 12))
+            .Return();
+
+        Assert.True(result.Failed);
+    }
+
+    [Theory]
+    [InlineData("user.name")]
+    [InlineData("user_name")]
+    [InlineData("username123")]
+    [InlineData("username.123")]
+    [InlineData("123username")]
+    [InlineData("123.username")]
+    [InlineData("UserName")]
+    [InlineData("Username")]
+    [InlineData("twelveschars")]
+    public void ValidUsernameTest(string username)
+    {
+        var result = Assertive.Result()
+            .Assert(x => x.Regex.Must(username).Validates.Username(3, 12))
+            .Return();
+
+        Assert.True(result.Success);
+    }
+}
