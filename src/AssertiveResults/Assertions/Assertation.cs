28c235271f2beb904545ea99eb901f6e690c6bb2
Taufan Augusta <taufan.augusta@protonmail.com>
2022-07-27 16:56:33 +0700

    add internal error code and input name

--- src/AssertiveResults/Assertions/Assertation.cs
+++ src/AssertiveResults/Assertions/Assertation.cs
@@ -6,9 +6,13 @@ namespace AssertiveResults.Assertions
 {
     public class Assertation : IAssertation
     {
+        private const string INPUT_NAME = "Input";
+
         public IAssertion Should { get; internal set; }
         public IRegex Regex { get; internal set; }
         internal List<Error> Errors { get; } = new List<Error>();
+        internal string InputName => INPUT_NAME;
+        internal string ErrorCode { get; set; }
         internal bool IsSatisfied { get; set; }
         internal bool Failed => Errors.Count > 0;
 
