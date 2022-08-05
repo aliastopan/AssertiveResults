0f43b6a736818eb6f0d0879a8ce7a455e7e5fce0
Taufan Augusta <taufan.augusta@protonmail.com>
2022-08-05 17:09:27 +0700

    add result overriding

--- src/AssertiveResults/Contracts/IResult.cs
+++ src/AssertiveResults/Contracts/IResult.cs
@@ -7,12 +7,15 @@ namespace AssertiveResults
     public interface IResult
     {
         IResult Assert(Action<IContext> context);
+        IResult<T> Override<T>();
         IAssertiveResult Resolve();
     }
 
     public interface IResult<T>
     {
         IResult<T> Assert(Action<IContext> context);
+        IResult<U> Override<U>();
+        IResult Override();
         IAssertiveResult<T> Resolve(Func<IResolve, T> result);
         IAssertiveResult<T> Resolve(ResolveBehavior resolveBehavior, Func<IResolve, T> result);
     }
