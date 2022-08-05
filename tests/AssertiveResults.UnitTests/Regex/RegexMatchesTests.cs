d1a0c46f06c6563447e37e6476b2aefbf86ce4c7
Taufan Augusta <taufan.augusta@protonmail.com>
2022-08-06 01:21:57 +0700

    tests

---
+++ tests/AssertiveResults.UnitTests/Regex/RegexMatchesTests.cs
@@ -0,0 +1,34 @@
+namespace AssertiveResults.UnitTests.Regex;
+
+public class RegexMatchesTests
+{
+    [Theory]
+    [InlineData("NOT_LOWERCASE")]
+    [InlineData("UPPERCASE!")]
+    [InlineData("UPPER123")]
+    public void MatchesUpperCaseTest(string input)
+    {
+       var result = Assertive.Result()
+            .Assert(x => {
+                x.RegularExpression.Validate(input).Matches("[A-Z]+");
+            })
+            .Resolve();
+        Assert.True(true);
+    }
+
+    [Theory]
+    [InlineData("lowercase")]
+    [InlineData("lowerUPPER")]
+    [InlineData("lower123")]
+    [InlineData("aA")]
+    public void MatchesLowerCaseTest(string input)
+    {
+        var result = Assertive.Result()
+            .Assert(x => {
+                x.RegularExpression.Validate(input).Matches("[a-z]+");
+            })
+            .Resolve();
+
+        Assert.True(result.Success);
+    }
+}
