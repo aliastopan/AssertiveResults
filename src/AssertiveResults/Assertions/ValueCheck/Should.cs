bd1f882564e897dcc584a31450dac400c3bc3400
Taufan Augusta <taufan.augusta@protonmail.com>
2022-08-11 22:53:03 +0700

    refactor: add ValueCheck error

--- src/AssertiveResults/Assertions/ValueCheck/Should.cs
+++ src/AssertiveResults/Assertions/ValueCheck/Should.cs
@@ -18,9 +18,9 @@ namespace AssertiveResults.Assertions.ValueCheck
             _context.IsSatisfied = condition;
             if(!_context.IsSatisfied)
             {
-                const string errorCode = "Boolean.Assertion";
+                const string errorCode = "Boolean.ValueCheck";
                 const string errorDescription = "Value must satisfy the specified condition.";
-                var error = Error.Assertion(errorCode, errorDescription);
+                var error = Error.ValueCheck(errorCode, errorDescription);
                 _context.Errors.Add(error);
             }
 
@@ -32,9 +32,9 @@ namespace AssertiveResults.Assertions.ValueCheck
             _context.IsSatisfied = !condition;
             if(!_context.IsSatisfied)
             {
-                const string errorCode = "Boolean.Assertion";
+                const string errorCode = "Boolean.ValueCheck";
                 const string errorDescription = "Value must not satisfy the illegal condition.";
-                var error = Error.Assertion(errorCode, errorDescription);
+                var error = Error.ValueCheck(errorCode, errorDescription);
                 _context.Errors.Add(error);
             }
 
@@ -46,9 +46,9 @@ namespace AssertiveResults.Assertions.ValueCheck
             _context.IsSatisfied = @object == null;
             if(!_context.IsSatisfied)
             {
-                const string errorCode = "Null.Assertion";
+                const string errorCode = "Null.ValueCheck";
                 const string errorDescription = "Value must be null.";
-                var error = Error.Assertion(errorCode, errorDescription);
+                var error = Error.ValueCheck(errorCode, errorDescription);
                 _context.Errors.Add(error);
             }
 
@@ -60,9 +60,9 @@ namespace AssertiveResults.Assertions.ValueCheck
             _context.IsSatisfied = @object != null;
             if(!_context.IsSatisfied)
             {
-                const string errorCode = "NotNull.Assertion";
+                const string errorCode = "NotNull.ValueCheck";
                 const string errorDescription = "Value must not be null.";
-                var error = Error.Assertion(errorCode, errorDescription);
+                var error = Error.ValueCheck(errorCode, errorDescription);
                 _context.Errors.Add(error);
             }
 
@@ -74,9 +74,9 @@ namespace AssertiveResults.Assertions.ValueCheck
             _context.IsSatisfied = !collection.GetEnumerator().MoveNext();
             if(!_context.IsSatisfied)
             {
-                const string errorCode = "Empty.Assertion";
+                const string errorCode = "Empty.ValueCheck";
                 const string errorDescription = "Value must be empty.";
-                var error = Error.Assertion(errorCode, errorDescription);
+                var error = Error.ValueCheck(errorCode, errorDescription);
                 _context.Errors.Add(error);
             }
 
@@ -88,9 +88,9 @@ namespace AssertiveResults.Assertions.ValueCheck
             _context.IsSatisfied = collection.GetEnumerator().MoveNext();
             if(!_context.IsSatisfied)
             {
-                const string errorCode = "NotEmpty.Assertion";
+                const string errorCode = "NotEmpty.ValueCheck";
                 const string errorDescription = "Value must not be empty.";
-                var error = Error.Assertion(errorCode, errorDescription);
+                var error = Error.ValueCheck(errorCode, errorDescription);
                 _context.Errors.Add(error);
             }
 
@@ -102,9 +102,9 @@ namespace AssertiveResults.Assertions.ValueCheck
             _context.IsSatisfied = former.Equals(latter);
             if(!_context.IsSatisfied)
             {
-                const string errorCode = "Equal.Assertion";
+                const string errorCode = "Equal.ValueCheck";
                 const string errorDescription = "Value(s) must be equal.";
-                var error = Error.Assertion(errorCode, errorDescription);
+                var error = Error.ValueCheck(errorCode, errorDescription);
                 _context.Errors.Add(error);
             }
 
@@ -116,9 +116,9 @@ namespace AssertiveResults.Assertions.ValueCheck
             _context.IsSatisfied = !former.Equals(latter);
             if(!_context.IsSatisfied)
             {
-                const string errorCode = "NotEqual.Assertion";
+                const string errorCode = "NotEqual.ValueCheck";
                 const string errorDescription = "Value(s) must not be equal.";
-                var error = Error.Assertion(errorCode, errorDescription);
+                var error = Error.ValueCheck(errorCode, errorDescription);
                 _context.Errors.Add(error);
             }
 
@@ -131,9 +131,9 @@ namespace AssertiveResults.Assertions.ValueCheck
             _context.IsSatisfied = result == 0;
             if(!_context.IsSatisfied)
             {
-                const string errorCode = "StrictEqual.Assertion";
+                const string errorCode = "StrictEqual.ValueCheck";
                 const string errorDescription = "Value(s) must have identical values.";
-                var error = Error.Assertion(errorCode, errorDescription);
+                var error = Error.ValueCheck(errorCode, errorDescription);
                 _context.Errors.Add(error);
             }
 
@@ -146,9 +146,9 @@ namespace AssertiveResults.Assertions.ValueCheck
             _context.IsSatisfied = result != 0;
             if(!_context.IsSatisfied)
             {
-                const string errorCode = "NotStrictEqual.Assertion";
+                const string errorCode = "NotStrictEqual.ValueCheck";
                 const string errorDescription = "Value(s) must not have identical values.";
-                var error = Error.Assertion(errorCode, errorDescription);
+                var error = Error.ValueCheck(errorCode, errorDescription);
                 _context.Errors.Add(error);
             }
             return this;
@@ -159,9 +159,9 @@ namespace AssertiveResults.Assertions.ValueCheck
             _context.IsSatisfied = former == latter;
             if(!_context.IsSatisfied)
             {
-                const string errorCode = "Equal.Assertion";
+                const string errorCode = "Equal.ValueCheck";
                 const string errorDescription = "Value(s) must be the same instance.";
-                var error = Error.Assertion(errorCode, errorDescription);
+                var error = Error.ValueCheck(errorCode, errorDescription);
                 _context.Errors.Add(error);
             }
 
@@ -173,9 +173,9 @@ namespace AssertiveResults.Assertions.ValueCheck
             _context.IsSatisfied = former != latter;
             if(!_context.IsSatisfied)
             {
-                const string errorCode = "Equal.Assertion";
+                const string errorCode = "Equal.ValueCheck";
                 const string errorDescription = "Value(s) must not be the same instance.";
-                var error = Error.Assertion(errorCode, errorDescription);
+                var error = Error.ValueCheck(errorCode, errorDescription);
                 _context.Errors.Add(error);
             }
 
