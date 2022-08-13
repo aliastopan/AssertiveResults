b92866c927bac5b6de3148e0d44237dd78f5ffee
Taufan Augusta <taufan.augusta@protonmail.com>
2022-08-14 04:10:17 +0700

    add error code references

---
+++ src/AssertiveResults/Errors/ErrorCode.cs
@@ -0,0 +1,23 @@
+namespace AssertiveResults.Errors
+{
+    internal static class ErrorCode
+    {
+        internal const string Failure = "General.Failure";
+        internal const string Conflict = "General.Conflict";
+        internal const string NotFound = "General.NotFound";
+        internal const string Unexpected = "General.Unexpected";
+        internal const string Validation = "General.Validation";
+
+        internal static class Assertion
+        {
+            internal const string Boolean = "Boolean.Assertion";
+            internal const string ObjectReference = "ObjectReference.Assertion";
+            internal const string Value = "Value.Assertion";
+            internal const string Collection = "Collection.Assertion";
+            internal const string RegularExpression = "RegularExpression.Validation";
+            internal const string UsernameStandard = "Username.Validation";
+            internal const string PasswordStrength = "PasswordStrength.Validation";
+            internal const string EmailAddressFormat = "EmailAddress.Validation";
+        }
+    }
+}
