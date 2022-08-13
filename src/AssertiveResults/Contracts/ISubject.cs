0a476a5a4238ba4328ea900fd03ece0e61df42a2
Taufan Augusta <taufan.augusta@protonmail.com>
2022-08-13 16:05:39 +0700

    chore: indent interfaces

--- src/AssertiveResults/Contracts/ISubject.cs
+++ src/AssertiveResults/Contracts/ISubject.cs
@@ -4,12 +4,18 @@ using AssertiveResults.Contracts;
 
 namespace AssertiveResults
 {
-    public interface ISubject : IOverload, IOverride, IResolve
+    public interface ISubject :
+        IResolve,
+        IOverload,
+        IOverride
     {
         ISubject Assert(Action<IContext> context);
     }
 
-    public interface ISubject<T> : IOverload<T>, IOverride<T>, IResolve<T>
+    public interface ISubject<T> :
+        IResolve<T>,
+        IOverload<T>,
+        IOverride<T>
     {
         ISubject<T> Assert(Action<IContext> context);
     }
--- src/AssertiveResults/IResult.cs
+++ src/AssertiveResults/IResult.cs
@@ -2,13 +2,22 @@ using AssertiveResults.Contracts;
 
 namespace AssertiveResults
 {
-    public interface IResult : IBegin, ISubject, IProblem, IMetadata, IInspect
+    public interface IResult :
+        IBegin,
+        ISubject,
+        IProblem,
+        IInspect,
+        IMetadata
     {
         bool IsSuccess { get; }
         bool HasFailed { get; }
     }
 
-    public interface IResult<T> : IResult, IBegin<T>, ISubject<T>, IMetadata<T>, IMatch<T>
+    public interface IResult<T> : IResult,
+        IBegin<T>,
+        ISubject<T>,
+        IMatch<T>,
+        IMetadata<T>
     {
         T Value { get; }
     }
